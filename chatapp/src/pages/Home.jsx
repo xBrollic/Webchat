b7f157e79f123d5e330b6f63ebe852965dcd54ee
gustavlundgren <sgvln005@edu.karlstad.se>
2023-01-30 22:32:24 +0100

    -

---
+++ chatapp/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <button>Get started</button>
+      <p>cool text av ingemar batman</p>
+    </main>
+  );
+}
+
+export default Home;
