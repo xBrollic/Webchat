4859e9cab67e118fc68af8be5a756bc6bb55c1ad
gustavlundgren <sgvln005@edu.karlstad.se>
2023-01-31 18:05:31 +0100

    -

---
+++ chatapp/src/components/PersistLogin.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+
+const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+
+  useEffect(() => {
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  }, []);
+
+  return <div></div>;
+};
+
+export default PersistLogin;
