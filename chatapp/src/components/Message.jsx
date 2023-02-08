bcb2b57404781d27ca51717a8d003bad5068e2cf
xBrollic <ingemar.barkman@icloud.com>
2023-02-08 12:08:29 +0100

    Right and left message

--- chatapp/src/components/Message.jsx
+++ chatapp/src/components/Message.jsx
@@ -1,36 +1,32 @@
 import React from "react";
+import useAuth from "../hooks/useAuth";
 
 const Message = ({ content, user, time }) => {
+  const { auth } = useAuth();
+  const style =
+    "w-fit p-1 rounded-xl ml-2 flex flex-col drop-shadow-xl mb-3";
+
   return (
-    <>
-      <p className="text-white text-opacity-70 ml-2">{user}</p>
-      <div className="bg-[#deacf5] w-fit p-1 rounded-xl ml-2 flex flex-col rounded-tl-sm drop-shadow-xl mb-3">
+    <main className="relative h-20">
+      {user === auth.user ? (
+        ""
+      ) : (
+        <p className="text-white text-opacity-70 ml-2">{user}</p>
+      )}
+      <div
+        className={
+          user === auth.user
+            ? `${style} rounded-tr-sm absolute right-3 bg-[#d781ff]`
+            : `${style} rounded-tl-sm absolute bg-[#deacf5]`
+        }
+      >
         <h3>{content}</h3>
         <p className="text-xs w-fit text-black text-opacity-60">{time}</p>
       </div>
-    </>
+    </main>
   );
 };
 
 export default Message;
 
-/* const [data, setData] = useState();
-
-const fetchMessages = () => {
-    try {
-      const response = axios.get(http//:localhost:3500/get-messages)
-      setData(response.body)
-    } catch (err) {
-      console.log(err)
-    }
-
-    ...
-
-    return(
-        <>
-            data.map(e => {
-                return <Message content={e.content} user={e.user} time={e.time} />
-            })
-        </>
-    )
-} */
+/*className={user === auth.user ? `${style} right bg-[mörk]` : `${style} left bg-[ljus]`}*/
